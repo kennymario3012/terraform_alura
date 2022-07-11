8b28f216fa429d8ca9a697752134a9aa7946e76c
Kenny Mário <kennymarioantonio@gmail.com>
2022-07-11 13:53:36 +0100

    feat: add dev6 intance

--- main.tf
+++ main.tf
@@ -4,6 +4,12 @@ provider "aws" {
 }
 
 
+provider "aws" {
+  alias = "us-east-2"
+  region = "us-east-2"
+}
+
+
 resource "aws_instance" "dev" {
   count = 3
   ami = "ami-052efd3df9dad4825"
@@ -12,22 +18,41 @@ resource "aws_instance" "dev" {
   tags = {
     "Name" = "dev${count.index}"
   }
-  vpc_security_group_ids = ["sg-06d346d91fcbb0c8d"]
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
 }
 
-resource "aws_security_group" "acesso-ssh" {
-  name= "acesso-ssh"
-  description = "acesso-ssh"
-  
-  ingress  {
-    from_port = "22"
-    to_port = "22"
-    protocol = "tcp"
 
-    cidr_blocks = ["197.216.105.46/32"]
-  }
+resource "aws_instance" "dev4" {
+  ami = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
+  key_name = "terraform_aws"
   tags = {
-    Name="ssh"
+    "Name" = "dev4"
   }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+  depends_on = [
+    aws_s3_bucket.dev4
+  ]
+}
 
+resource "aws_instance" "dev5" {
+  ami = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
+  key_name = "terraform_aws"
+  tags = {
+    "Name" = "dev5"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+
+
+resource "aws_instance" "dev6" {
+  provider = aws.us-east-2
+  ami = "ami-02d1e544b84bf7502"
+  instance_type = "t2.micro"
+  key_name = "terraform_aws"
+  tags = {
+    "Name" = "dev6"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}"]
 }
